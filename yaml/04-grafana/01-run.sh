0689489eca2a9bb1e3ae37af6e96908dde4679e2
Justin Paul <justin@jpaul.me>
2024-07-09 14:58:26 -0400

    grafana-provisioning branch

--- yaml/04-grafana/01-run.sh
+++ yaml/04-grafana/01-run.sh
@@ -1,8 +1,10 @@
 # Apply the configuration to the default namespace
 kubectl apply -f grafana-secret.yaml --namespace=default
 
-# Apply the configuration to the default namespace
+# Apply the config maps to the default namespace
 kubectl apply -f configmaps.yaml --namespace=default
+
+kubectl apply -f provisioning-config-map.yaml
 
 # Apply the configuration to the default namespace
 kubectl apply -f deployment.yaml --namespace=default
